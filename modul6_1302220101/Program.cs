e39d325f428948cf8959b61219e6d955ce4e2c4f
Yoga Fikri <yogarz354@gmail.com>
2024-03-30 08:31:24 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- modul6_1302220101/Program.cs
+++ modul6_1302220101/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SayaTubeUser User = new SayaTubeUser("Yoga");
+
+            SayaTubeVideo vid1 = new SayaTubeVideo("Casino Royale oleh Yoga");
+            SayaTubeVideo vid2 = new SayaTubeVideo("Avatar oleh Yoga");
+            SayaTubeVideo vid3 = new SayaTubeVideo("Parasite oleh Yoga");
+            SayaTubeVideo vid4 = new SayaTubeVideo("Come and See oleh Yoga");
+            SayaTubeVideo vid5 = new SayaTubeVideo("Harakiri oleh Yoga");
+            SayaTubeVideo vid6 = new SayaTubeVideo("The Godfather oleh Yoga");
+            SayaTubeVideo vid7 = new SayaTubeVideo("Seven Samurai oleh Yoga");
+            SayaTubeVideo vid8 = new SayaTubeVideo("Spirited Away oleh Yoga");
+            SayaTubeVideo vid9 = new SayaTubeVideo("Spider-Man oleh Yoga");
+            SayaTubeVideo vid10 = new SayaTubeVideo("The Dark Knight oleh Yoga");
+
+            User.addVideo(vid1);
+            User.addVideo(vid2);
+            User.addVideo(vid3);
+            User.addVideo(vid4);
+            User.addVideo(vid5);
+            User.addVideo(vid6);
+            User.addVideo(vid7);
+            User.addVideo(vid8);
+            User.addVideo(vid9);
+            User.addVideo(vid10);
+
+            vid1.increasePlayCount(39);
+            vid1.printVideoDetails();
+            Console.WriteLine();
+            User.PrintAllVideoPlaycount();
         }
     }
 }
---
+++ modul6_1302220101/SayaTubeVideo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302220101
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int playCount;
+
+        public SayaTubeVideo(String judul)
+        {
+            this.title = judul;
+            playCount = 0;
+            Random rd = new Random();
+            id = rd.Next(10000, 99999);
+        }
+
+        public String getTitle()
+        {
+            return title;
+        }
+
+        public int getPlayCount() { return playCount;}
+
+        public void increasePlayCount(int inputPlayCount)
+        {
+            playCount += inputPlayCount;
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine($"id: {id}");
+            Console.WriteLine($"title: {title}");
+            Console.WriteLine($"playCount: {playCount}");
+        }
+    }
+}
